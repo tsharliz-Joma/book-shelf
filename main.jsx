317c9453f86c2fd63d4366fe38bee124ef72a645
charles-1036484-joma <1036484@student.sae.edu.au>
2026-09-16 18:40:11 +1000

    Add tailwind

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import BookShelfLibrary from "./book-shelf.jsx";
+import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./main.jsx", "./book-shelf.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
